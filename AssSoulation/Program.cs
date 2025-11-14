8edf04e0a56107e651cf6f1dcd56daaa94763362
unknown <bhaatiti281@gmail.com>
2025-11-14 23:14:45 +0200

    Sec

--- AssSoulation/Program.cs
+++ AssSoulation/Program.cs
@@ -3,7 +3,7 @@ using static DemoG01.Data.ListGenerator;
 
 namespace AssSoulation
 {
-    internal class Program 
+    internal class Program
     {
         static void Main(string[] args)
         {
@@ -109,6 +109,114 @@ namespace AssSoulation
             //Console.WriteLine(string.Join("\n" , res));
 
             #endregion
+
+            #region Q12
+            //var res = ProductList?.GroupBy(p => p.Category).Select(c => new { c.Key , MaxPrice= c.Max(p=>p.UnitPrice) });
+            //Console.WriteLine(string.Join("\n" , res));
+            #endregion
+            #region Q13
+
+
+            //var res = ProductList
+            //    .GroupBy(p => p.Category)
+            //    .Select(g => g.OrderByDescending(p => p.UnitPrice).First());
+
+            #endregion
+
+            #region Q14
+            //var res = ProductList?.GroupBy(p => p.Category).Select(c => new { c.Key, MaxPrice = c.Average(p => p.UnitPrice) });
+            //Console.WriteLine(string.Join("\n", res));
+
+            #endregion
+
+
+
+            #endregion
+
+            #region Set Operators
+
+            #region Q1
+            //var res = ProductList?.Select(p => p.Category).Distinct();
+            //Console.WriteLine(String.Join("\n" , res));
+            #endregion
+
+
+            #region Q2
+            //var res = ProductList?.Select(p => p.ProductName.ElementAt(0))
+            //    .Union(CustomerList?.Select(c => c.CustomerName.ElementAt(0)));
+            //Console.WriteLine(String.Join("\n" , res));
+            #endregion
+
+
+            #region Q3
+
+            //var res = ProductList?.Select(p => p.ProductName.ElementAt(0))
+            //    .Intersect(CustomerList?.Select(c => c.CustomerName.ElementAt(0)));
+            //Console.WriteLine(String.Join("\n" , res));
+
+            #endregion
+
+
+            #region Q4
+            //        var res4 = ProductList?.Select(p => p.ProductName[0])
+            //.Except(CustomerList?.Select(c => c.CustomerName[0]));
+            #endregion
+
+
+            #region Q5
+            //var res = ProductList?.Select(p => p.ProductName.Length > 3 ? p.ProductName.Substring(p.ProductName.Length - 3) : p.ProductName)
+            //   .Concat(CustomerList?.Select(c =>    c.CustomerName.Length > 3  ? c.CustomerName.Substring(c.CustomerName.Length - 3) : c.CustomerName));
+            //Console.WriteLine(String.Join("\n", res));
+            #endregion
+
+
+
+
+
+            #endregion
+
+            #region Partitioning Operators
+
+            #region Q1
+
+            //var res = CustomerList.Where(c => c.City.Equals("Washington", StringComparison.OrdinalIgnoreCase)).
+            //    SelectMany(o => o.Orders).Take(3) ;
+
+            //Console.WriteLine(String.Join("\n", res));
+            #endregion
+
+            #region Q2
+            //var res = CustomerList.Where(c => c.City.Equals("Washington", StringComparison.OrdinalIgnoreCase)).
+            //    SelectMany(o => o.Orders).Skip(2) ;
+            #endregion
+
+            #region Q3
+
+
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var res = numbers.TakeWhile((n,i) => n > i);
+
+            #endregion
+            #region Q4
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var res = numbers.SkipWhile((n,i) => n %3==0 );
+            #endregion
+            #region Q5
+
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var res = numbers.SkipWhile((n, i) => n >= i);
+
+            #endregion
+
+
+
+
+            #endregion
+
+            #region Quantifiers
+            var wrods = File.ReadAllLines("dictionary_english.txt");
+            var res= wrods.Where(w => w.Contains("ei" , StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(String.Join("\n", res));
             #endregion
 
 
